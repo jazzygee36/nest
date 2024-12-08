42c51055bfc38fb5cd4f40d411d6846d406481e2
Samson Mesioye <jazzygee36gmail.com>
2024-12-08 21:11:43 +0100

    users

---
+++ src/users/users.controller.ts
@@ -0,0 +1,37 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+  @Get()
+  findAll() {
+    return [];
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post()
+  create(@Body() user: []) {
+    return user;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUser: {}) {
+    return { id, updateUser };
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id') id: string) {
+    return { id };
+  }
+}
